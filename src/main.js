cd2f814b69117be5029177a95de49d84950ee14c
nikkifang <nikkifang@tencent.com>
2026-03-20 11:04:12 +0800

    init

---
+++ src/main.js
@@ -0,0 +1,4 @@
+import '@fontsource-variable/noto-sans-sc'
+import '@fontsource-variable/noto-serif-sc'
+import '@fontsource/space-mono/400.css'
+import '@fontsource/space-mono/700.css'
---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+
+export default defineConfig({
+  root: 'src',
+  publicDir: resolve(__dirname, 'public'),
+  build: {
+    outDir: resolve(__dirname, 'dist'),
+    emptyOutDir: true,
+  },
+})
